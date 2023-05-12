450988e5654bea1058579952df9117221574897c
Tisauuur <intfjgp@protonmail.com>
2023-05-12 03:50:57 -0400

    modified:   pdf-ai-assistant.py

--- pdf-ai-assistant.py
+++ pdf-ai-assistant.py
@@ -47,7 +47,7 @@ def main():
                         docs = knowledge_base.similarity_search(user_question)
 
                         
-                        llm = OpenAI(model_name="gpt-3.5-turbo")
+                        llm = OpenAI()
                         chain = load_qa_chain(llm, chain_type="stuff")
                         with get_openai_callback() as cb:
                                 response = chain.run(input_documents=docs, question=user_question)
